3f8a7f2be69550fa29e87e4d1750ea977b046d8e
Tessa <32836374+tgsy@users.noreply.github.com>
2018-03-09 01:46:55 +0800

    added company activity

---
+++ app/src/main/java/com/example/tessa/kyc/CompanyActivity.java
@@ -0,0 +1,13 @@
+package com.example.tessa.kyc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CompanyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company);
+    }
+}
