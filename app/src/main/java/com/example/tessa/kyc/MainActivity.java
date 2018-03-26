62d7520c148ff333aeb7675d20e55c005c684a29
Tessa <32836374+tgsy@users.noreply.github.com>
2018-03-26 23:56:40 +0800

    added new Read and Write NFCFragments, added MainActivity shared preferences, replaced signedup shared preferences with firebase, changed company logo drawables to firebase storage access

--- app/src/main/java/com/example/tessa/kyc/MainActivity.java
+++ app/src/main/java/com/example/tessa/kyc/MainActivity.java
@@ -1,19 +1,20 @@
 package com.example.tessa.kyc;
 
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
 public class MainActivity extends BaseActivity {
 
-    private FirebaseAuth mAuth;
+    private SharedPreferences sharedPref;
 
-    final String TAG = "DED";
+    private SharedPreferences.Editor editor;
+
+    private boolean previouslyStarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,32 +22,54 @@ public class MainActivity extends BaseActivity {
         setContentView(R.layout.activity_main);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        sharedPref = getBaseContext().getSharedPreferences(
+                getString(R.string.preference_getting_started), Context.MODE_PRIVATE);
+        editor = sharedPref.edit();
+
+        previouslyStarted = sharedPref.getBoolean(getString(R.string.preference_getting_started), false);
+        Log.i("ONSTART", "previouslyStarted = "+ previouslyStarted);
+
+        if (!previouslyStarted) {
+            editor.putBoolean(getString(R.string.preference_getting_started), Boolean.TRUE);
+            editor.apply();
+        } else
+            moveOn();
+    }
+
     public void onGetStarted(View view){
+        moveOn();
+    }
+
+    public void moveOn() {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
     }
 
-//    public void onLogin(View view){
-//        Intent intent = new Intent(this, SignUpActivity.class);
-//        startActivity(intent);
-//    }
+/*    public void onLogin(View view){
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
+    }
 
-//    @Override
-//    public void onStart() {
-//        super.onStart();
-//        // Check if user is signed in (non-null) and update UI accordingly.
-//        FirebaseUser currentUser = mAuth.getCurrentUser();
-//        Log.i(TAG, "signed in");
-//        if (currentUser!=null) updateUI(currentUser);
-//        else onResume();
-//    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        Log.i(TAG, "signed in");
+        if (currentUser!=null) updateUI(currentUser);
+        else onResume();
+    }*/
 
-    private void updateUI(FirebaseUser user) {
+/*    private void updateUI(FirebaseUser user) {
         hideProgressDialog();
         if (user != null) {
             Intent intent = new Intent(this, LoginActivity.class);
             startActivity(intent);
         }
-    }
+    }*/
 
 }
